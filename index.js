c855ce6e32e3a6974ac6e535ecabf00b32986223
5kilian <5kilian@informatik.uni-hamburg.de>
2019-07-14 14:15:32 +0200

    Add simple removeLabel when pr closed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const keywords = require("./keywords");
+const keywords = require('./keywords');
 
-async function addLabel(context) {
+async function addLabel (context) {
   console.log(context.payload.action, context.payload.pull_request.body);
 
   const issues = keywords(context.payload.pull_request.body);
@@ -9,30 +9,38 @@ async function addLabel(context) {
     await issues.forEach(async issue => {
       const params = context.issue({
         number: issue,
-        labels: ["in-progress"]
+        labels: ['in-progress']
       });
       await context.github.issues.addLabels(params);
     });
   }
-  return;
+}
+
+async function removeLabel (context) {
+  // review body information for the fixes, fixed, etc keywords.
+  const issues = keywords(context.payload.pull_request.body);
+
+  if (issues.length > 0) {
+    await issues.forEach(async issue => {
+      const params = context.issue({
+        number: issue,
+        name: 'in-progress',
+      });
+
+      await context.github.issues.removeLabel(params);
+    });
+  }
 }
 
 module.exports = robot => {
   // Your code here
-  console.log("Yay, the app was loaded!");
+  console.log('Yay, the app was loaded!');
 
-  robot.on("pull_request.opened", addLabel);
+  robot.on('pull_request.opened', addLabel);
 
-  robot.on("pull_request.reopened", addLabel);
+  robot.on('pull_request.reopened', addLabel);
 
-  robot.on("pull_request.editedf", addLabel);
+  robot.on('pull_request.edited', addLabel);
 
-  robot.on("pull_request.closed", async context => {
-    console.log("CLOSED");
-    // Once the PR is closed we want to remove our label from the
-    // issues we had assigned as in progress
-    // 1. Check that the issues were marked as fixed?
-    // 2. For each issue remove the label
-    return;
-  });
+  robot.on('pull_request.closed', removeLabel);
 };
